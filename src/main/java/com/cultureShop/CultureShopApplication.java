89eb26019424060494e7ba4492e1ce6b7a5646ff
“poogeun” <hello.git.jiy2797@naver.com>
2024-07-29 17:33:58 +0900

    START

---
+++ src/main/java/com/cultureShop/CultureShopApplication.java
@@ -0,0 +1,13 @@
+package com.cultureShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultureShop/CultureShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultureShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
