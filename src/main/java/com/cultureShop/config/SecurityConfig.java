2563b0950d3779ffc50f2aba03cc82a979deb27c
poogeun <hello.git.jiy2797@naver.com>
2024-08-01 07:44:08 +0900

    Feat: header 작업중

--- src/main/java/com/cultureShop/config/SecurityConfig.java
+++ src/main/java/com/cultureShop/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(auth -> auth
                 .requestMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico", "/error").permitAll()
-                .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .requestMatchers("/", "/members/**", "/item/**", "/image/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
         );
