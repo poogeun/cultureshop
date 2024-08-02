8a5133033f175810222d6155ea52b39d1035e562
“poogeun” <hello.git.jiy2797@naver.com>
2024-08-02 18:15:41 +0900

    Feat: 회원가입 만드는중

--- src/main/java/com/cultureShop/controller/MemberController.java
+++ src/main/java/com/cultureShop/controller/MemberController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/members")
 @Controller
@@ -27,9 +29,25 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String memberForm(@Valid MemberFormDto memberFormDto){
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String memberForm(@Valid MemberFormDto memberFormDto, @RequestParam("checkYn") String checkYn,
+                             BindingResult bindingResult, Model model){
+        System.out.println("회원가입 클릭");
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+        if(checkYn.equals("N")){
+            model.addAttribute("errorMessage", "개인정보수집 및 이용동의는 필수 항목입니다.");
+            return "member/memberForm";
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }
+        catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 }
--- src/main/java/com/cultureShop/dto/MemberFormDto.java
+++ src/main/java/com/cultureShop/dto/MemberFormDto.java
@@ -1,17 +1,35 @@
 package com.cultureShop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class MemberFormDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자이상, 16자 이하로 입력해주세요.")
     private String password;
+
+    @NotEmpty(message = "비밀번호 확인은 필수 입력 값입니다.")
     private String passwordCk;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
+
+    @NotBlank(message = "전화번호는 필수 입력 값입니다.")
     private String tel;
+
     private String checkYn; // 개인정보이용 동의
 }
